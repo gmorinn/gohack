06f4b23d7ddc54a8238f49d80ad996b08753883d
Guillaume MORIN <guillaume.morin@epitech.eu>
2022-06-27 15:06:37 +0400

    scan_port && crack md5 && crach sha256

---
+++ crack_md5_sha256/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	_sha256hash string // "95a5e1547df73abdd4781b6c9e55f3377c15d08884b11738c2727dbd887d4ced"
+	_md5hash    string // "77f62e3524cd583d698d51fa24fdff4f"
+	_wordlist   string
+)
+
+func init() {
+	flag.StringVar(&_wordlist, "w", "wordlist.txt", "wordlist is the name of the file default is wordlist.txt")
+	flag.StringVar(&_sha256hash, "sha256", "", "sha256 hash")
+	flag.StringVar(&_md5hash, "md5", "", "md5 hash")
+	flag.Parse()
+}
+
+func open_file(path string) *os.File {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
+
+func check_password_md5(password string) {
+	hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
+	if hash == _md5hash {
+		fmt.Printf("[+] Password found (MD5): %s\n", password)
+		os.Exit(0)
+	}
+}
+
+func check_password_sha_256(password string) {
+	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+	if hash == _sha256hash {
+		fmt.Printf("[+] Password found (SHA-256): %s\n", password)
+		os.Exit(0)
+	}
+}
+
+func main() {
+	// open file
+	file := open_file(_wordlist)
+	defer file.Close()
+
+	// check password
+	if _sha256hash == "" && _md5hash == "" {
+		log.Println("[!] No hash provided")
+		os.Exit(84)
+	}
+
+	// loop wordlist
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		password := scanner.Text()
+		if _md5hash != "" {
+			check_password_md5(password)
+		}
+		if _sha256hash != "" {
+			check_password_sha_256(password)
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatalln(err)
+		}
+	}
+	fmt.Println("[!] No Password found")
+}
---
+++ scan_port/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+var (
+	_host       string
+	_nb_workers int
+	_channels   chan int
+)
+
+func init() {
+	flag.StringVar(&_host, "h", "127.0.0.1", "machine is the name of the machine default is 127.0.0.1")
+	flag.IntVar(&_nb_workers, "w", 10, "number of workers (default 10)")
+	flag.Parse()
+}
+
+// Take a port number as argument and try to connect to it with the hostname given in the -h flag.
+func connectTCP(port int) {
+	c, err := net.Dial("tcp", fmt.Sprintf("%s:%d", _host, port))
+	if err != nil {
+		return
+	}
+	fmt.Println("Connection successful in port ", port)
+	if err = c.Close(); err != nil {
+		fmt.Println("Error closing connection: ", err)
+		return
+	}
+}
+
+func worker() {
+	for p := range _channels {
+		connectTCP(p)
+	}
+}
+
+func main() {
+	// Create a channel to communicate with the workers
+	_channels = make(chan int, _nb_workers)
+
+	if _nb_workers <= 0 {
+		_nb_workers = 10
+	}
+
+	// Start the workers
+	for i := 0; i < cap(_channels); i++ {
+		go worker()
+	}
+
+	// Send the ports to the workers
+	for i := 1; i <= 3000; i++ {
+		_channels <- i
+	}
+
+	close(_channels)
+}
